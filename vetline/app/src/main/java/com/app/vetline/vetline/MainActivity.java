c65a379970d18eed8fe5c600d18b2cfbf4f0a2f7
Matheus Bento <knightadm@hotmail.com>
2016-12-06 09:59:49 -0200

    Add files via upload

--- vetline/app/src/main/java/com/app/vetline/vetline/MainActivity.java
+++ vetline/app/src/main/java/com/app/vetline/vetline/MainActivity.java
@@ -1,7 +1,10 @@
 package com.app.vetline.vetline;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    Button btn_pagina_principal1 = (Button)findViewById(R.id.btn_pagina_principal);
+    Button btn_cadastrar_animal1 = (Button) findViewById(R.id.btn_cadastrar_animal);
+    Button btn_cadastrar_consulta1 = (Button) findViewById(R.id.btn_cadastrar_consulta);
+    Button btn_cadastrar_usuario1 = (Button) findViewById(R.id.btn_cadastrar_usuario);
+    Button btn_edicao_animal = (Button) findViewById(R.id.btn_edicao_animal);
+
+    btn_pagina_principal1
+
+    Intent tela_cadastro_animal = new Intent(this,Tela_pagina_principal.class);
+
 }
