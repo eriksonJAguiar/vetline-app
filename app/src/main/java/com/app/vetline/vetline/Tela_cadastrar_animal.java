2fb64e1bdd94b0b150cdccaf0a2a4cb9c370b5bb
eduardopac <eduardopacheco0306@gmail.com>
2016-12-10 18:45:53 -0200

    classesTelaCadastrarAnimalAtualizadas

--- app/src/main/java/com/app/vetline/vetline/Tela_cadastrar_animal.java
+++ app/src/main/java/com/app/vetline/vetline/Tela_cadastrar_animal.java
@@ -1,12 +1,24 @@
 package com.app.vetline.vetline;
 
+import android.animation.ValueAnimator;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import controller.CCadastrarAnimal;
+import controller.CCadastrarCliente;
+import model.Animal;
+import model.Cliente;
 
 /**
  * Created by ProBook on 06/12/2016.
@@ -19,6 +31,7 @@ public class Tela_cadastrar_animal extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tela_cadastrar_animal);
         getSupportActionBar().hide();
+
 
         List<String> tamanhos = new ArrayList<String>();
         tamanhos.add("Pequeno");
@@ -41,5 +54,104 @@ public class Tela_cadastrar_animal extends AppCompatActivity {
         spinnerArrayAdapter1.setDropDownViewResource(android.R.layout.simple_spinner_item);
         spn11.setAdapter(spinnerArrayAdapter1);
 
+        Button btn_cadastrarAnimal = (Button) findViewById(R.id.btn_cadastraranimal);
+        btn_cadastrarAnimal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Animal animalOb;
+                EditText nomeAnimal = (EditText) findViewById(R.id.campo_nomeAnimal);
+                EditText numPedigree = (EditText) findViewById(R.id.campo_num_pedigree);
+                EditText raca = (EditText) findViewById(R.id.campo_raca);
+                Spinner tamanho = (Spinner) findViewById(R.id.campo_tamanho);
+                Spinner genero = (Spinner) findViewById(R.id.campo_genero);
+
+                String nomeAnimalStr = nomeAnimal.getText().toString();
+                String numPedigrees = numPedigree.getText().toString();
+                String racaStr = raca.getText().toString();
+                String tamanhoStr = tamanho.getSelectedItem().toString();
+                String generoStr = genero.getSelectedItem().toString();
+
+                try {
+                    animalOb = new Animal();
+
+                    animalOb.setNome(nomeAnimalStr);
+                    if (animalOb.getNome().isEmpty()) {
+                        throw new Exception("Nome vazio!");
+                    }
+                    animalOb.setPedigree(Integer.parseInt(numPedigrees));
+                    if (animalOb.getPedigree() == 0) {
+                        throw new Exception("Numero vazio!");
+                    }
+                    animalOb.setRaca(racaStr);
+                    if (animalOb.getRaca().isEmpty()) {
+                        throw new Exception("raca vazio!");
+                    }
+                    animalOb.setGenero(generoStr);
+                    if (animalOb.getGenero().isEmpty()) {
+                        throw new Exception("Endereço vazio!");
+                    }
+                    animalOb.setTamanho(tamanhoStr);
+                    if (animalOb.getTamanho().isEmpty()) {
+                        throw new Exception("Numero vazio!");
+                    }
+
+
+                    CCadastrarAnimal CCadastrarAnimal = new CCadastrarAnimal();
+                    if (CCadastrarAnimal.CadastrarAnimal(animalOb)) {
+                        final LinearLayout camposVet2 = (LinearLayout) findViewById(R.id.mensagens);
+                        camposVet2.setVisibility(View.VISIBLE);
+                        camposVet2.setBackgroundResource(R.drawable.screen_border_sucesso);
+                        camposVet2.setMinimumHeight(210);
+                        TextView msg = (TextView) findViewById(R.id.msg);
+                        msg.setTextColor(Color.parseColor("#3c763d"));
+                        msg.setText("SUCESSO: Cadastrado com sucesso");
+
+                        final View animatedView2 = (View) findViewById(R.id.btn_cadastar);
+                        final LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) animatedView2.getLayoutParams();
+                        ValueAnimator animator = ValueAnimator.ofInt(params.topMargin, params.WRAP_CONTENT);
+                        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                            @Override
+                            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                                params.topMargin = (Integer) valueAnimator.getAnimatedValue();
+                                animatedView2.requestLayout();
+                            }
+                        });
+                        animator.setDuration(300);
+                        animator.start();
+                    }
+                else {
+                    try {
+                        throw new Exception("Nao e possivel cadastrar");
+                    } catch (Exception e1) {
+                        e1.printStackTrace();
+                    }
+                }
+
+                }catch (Exception e){
+                    final LinearLayout camposVet2 = (LinearLayout) findViewById(R.id.mensagens);
+                    camposVet2.setVisibility(View.VISIBLE);
+                    camposVet2.setBackgroundResource(R.drawable.screen_border_falha);
+                    camposVet2.setMinimumHeight(210);
+                    TextView msg = (TextView) findViewById(R.id.msg);
+                    msg.setTextColor(Color.parseColor("#a94442"));
+                    msg.setText("ERROR: "+e.getMessage());
+
+                    final View animatedView2 = (View) findViewById(R.id.btn_cadastar);
+                    final LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) animatedView2.getLayoutParams();
+                    ValueAnimator animator = ValueAnimator.ofInt(params.topMargin, params.WRAP_CONTENT);
+                    animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                        @Override
+                        public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                            params.topMargin = (Integer) valueAnimator.getAnimatedValue();
+                            animatedView2.requestLayout();
+                        }
+                    });
+                    animator.setDuration(300);
+                    animator.start();
+                }
+
+
+            }
+        });
     }
 }
