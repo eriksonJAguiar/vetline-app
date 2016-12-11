bfdcd8c975dcd1a4363cc7db76c324c175bcc893
eduardopac <eduardopacheco0306@gmail.com>
2016-12-11 05:57:17 -0200

    telasVacina e alergia Atualizadas

--- app/src/main/java/com/app/vetline/vetline/Tela_cadastrar_alergia.java
+++ app/src/main/java/com/app/vetline/vetline/Tela_cadastrar_alergia.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import controller.CCadastrarAlergia;
+import model.Alergia;
 
 /**
  * Created by ProBook on 06/12/2016.
@@ -17,17 +21,23 @@ public class Tela_cadastrar_alergia extends AppCompatActivity {
         setContentView(R.layout.tela_cadastrar_alergia);
         getSupportActionBar().hide();
 
-        /* Carregar uma listsView com as Clinicas no Banco
-        *
-        *
-        *
-         */
 
-        Button adicionarClinica = (Button) findViewById(R.id.btn_cadastar);
+
+        Button adicionarClinica = (Button) findViewById(R.id.btn_cadastarAlergia);
         adicionarClinica.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //ir para a tela de adicionar uma nova clinica
+                Alergia al = new Alergia();
+                CCadastrarAlergia cCadastrarAlergia = new CCadastrarAlergia();
+
+                EditText alergia = (EditText) findViewById(R.id.campo_Alergia);
+                EditText descricao = (EditText) findViewById(R.id.campo_descricaoAlergia);
+
+                al.setAlergia(alergia.getText().toString());
+                al.setDescricao(alergia.getText().toString());
+
+                cCadastrarAlergia.inserirAlergia(al);
+
             }
         });
     }
--- app/src/main/java/com/app/vetline/vetline/Tela_cadastrar_vacina.java
+++ app/src/main/java/com/app/vetline/vetline/Tela_cadastrar_vacina.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import controller.CCadastrarVacina;
+import model.Vacina;
 
 /**
  * Created by ProBook on 06/12/2016.
@@ -17,17 +21,23 @@ public class Tela_cadastrar_vacina extends AppCompatActivity {
         setContentView(R.layout.tela_cadastrar_vacina);
         getSupportActionBar().hide();
 
-        /* Carregar uma listsView com as Clinicas no Banco
-        *
-        *
-        *
-         */
-
-        Button adicionarClinica = (Button) findViewById(R.id.btn_cadastar);
+        Button adicionarClinica = (Button) findViewById(R.id.btn_cadastarVacina);
         adicionarClinica.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //ir para a tela de adicionar uma nova clinica
+                Vacina vac = new Vacina();
+                CCadastrarVacina cCadastrarVacina = new CCadastrarVacina();
+
+                EditText vacina = (EditText) findViewById(R.id.campo_vacina);
+                EditText data = (EditText) findViewById(R.id.campo_dataVacina);
+                EditText descricao = (EditText) findViewById(R.id.campo_descricaoVacina);
+                EditText validade = (EditText) findViewById(R.id.campo_validade);
+
+                vac.setDescricao(vacina.getText().toString());
+                vac.setData(data.getText().toString());
+                vac.setDescricao(descricao.getText().toString());
+                vac.setValidaAte(validade.getText().toString());
+
             }
         });
     }
